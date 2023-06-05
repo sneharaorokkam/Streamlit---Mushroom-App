78f7b21ea361163d8e44d6969c6ff52ff7d4b86a
Sneha Rao <sneharao.693@gmail.com>
2023-06-05 16:32:06 -0400

    Updated Requirements.txt and Dataset File path

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import warnings
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -8,6 +9,10 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
 from sklearn.metrics import precision_score, recall_score 
+
+warnings.filterwarnings('ignore')
+warnings.simplefilter('ignore')
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 def main():
     st.title("Binary Classification Web App")
@@ -17,7 +22,7 @@ def main():
 
     @st.cache(persist = True)
     def load_data():
-        data = pd.read_csv('/home/rhyme/Desktop/Project/mushrooms.csv')
+        data = pd.read_csv('mushrooms.csv')
         label = LabelEncoder()
         for col in data.columns:
             data[col] = label.fit_transform(data[col])
